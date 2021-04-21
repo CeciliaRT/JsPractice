eb500a72f1a1da07d0b3d1e2bf869ff2d0f96b50
Cecilia <cecilia.rodriguez@scoperetail.com>
2021-04-20 21:38:01 -0500

    ok

--- burbuja.js
+++ burbuja.js
@@ -2,17 +2,15 @@ function bubbleSort(items){
     const elementos = items.length
 
     for(var i = (elementos-1); i>0; i--){
-        for(var j=(elementos-i); j>0; j--){
-            if(items[j] < items[j-1]){
-                [items[j], items[j-1] =[items[[items[j-1]], [items[j]]]
+        for(var j = (elementos-i); j>0; j--){
+            if(items[j] < items [j-1]){
+                [items[j],items[j-1]]= [items [j -1],items[j]]
             }
 
         }
     }
-
-
-    return items;
-};
+    return items
+}
 
 var array = [8,9,3,5,1]
 console.log(bubbleSort(array))
